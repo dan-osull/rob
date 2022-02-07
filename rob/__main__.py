64cc6183a39686de5f6c8c85b8bc25df19ec8a6b
dan-osull <47532371+dan-osull@users.noreply.github.com>
2022-02-07 17:02:40 -0600

    Update __main__.py

--- rob/__main__.py
+++ rob/__main__.py
@@ -1,8 +1,7 @@
 from rob.cli import cli
 from rob.console import print_
 
-if __name__ == "__main__":
-    # Entry point for application
-    cli()  # pylint: disable=no-value-for-parameter
-    # TODO: should we try to clean up library and filesystem if left in an inconsistent state?
-    print_("")
+# Entry point for application
+cli()  # pylint: disable=no-value-for-parameter
+# TODO: should we try to clean up library and filesystem if left in an inconsistent state?
+print_("")
